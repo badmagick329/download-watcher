26f972ed44973c741774e5e171a9fbbd29104bd3
badmagick329 <uzairfarooqi@gmail.com>
2024-10-01 20:23:54 +0100

    create target dir if it doesn't exist

--- src/DownloadWatcher.Application/Application.cs
+++ src/DownloadWatcher.Application/Application.cs
@@ -17,12 +17,17 @@ public class Application
         }
         try
         {
+            DirectoryInfo? parent = Directory.GetParent(newName);
+            if (parent != null && !Path.Exists(parent.FullName))
+            {
+                Directory.CreateDirectory(parent.FullName);
+            }
             Console.WriteLine($"Moving file to {newName}");
             File.Move(path, newName);
         }
         catch (Exception e)
         {
-            Console.WriteLine($"Error moving file: {e.Message} {e}");
+            Console.WriteLine($"Error moving file: {e.Message}");
         }
     }
 }
