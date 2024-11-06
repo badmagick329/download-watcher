35363c2fb49abbfc91e8c6c4baaa68518a67e641
badmagick329 <uzairfarooqi@gmail.com>
2024-11-06 09:29:47 +0000

    refactor

--- src/DownloadWatcher.Console/Program.cs
+++ src/DownloadWatcher.Console/Program.cs
@@ -7,7 +7,8 @@ class Program
         string[] fullArgs = Environment.GetCommandLineArgs();
         if (fullArgs.Length < 2)
         {
-            string helpMessage = @$"Usage: {fullArgs[0]} <Download Folder> [Rules File]
+            string helpMessage =
+                @$"Usage: {fullArgs[0]} <Download Folder> [Rules File]
 
 Arguments:
   <Download Folder>  The path to the folder where downloads are stored. This argument is required.
@@ -40,43 +41,38 @@ Arguments:
         else
         {
             Run(fullArgs[1], rulesFile);
-
         }
-
     }
-
 
     public static void Run(string downloadDir, string rulesFile)
     {
-        FileSystemWatcher watcher = new()
-        {
-            Path = downloadDir,
-            NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
-            Filter = "*.*",
-        };
+        FileSystemWatcher watcher =
+            new()
+            {
+                Path = downloadDir,
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                Filter = "*.*",
+            };
         RulesText rulesText = new(rulesFile);
         Application app = new(rulesText.Text);
         watcher.Created += (source, e) => OnCreate(source, e, app);
         watcher.Renamed += (source, e) => OnChange(source, e, app);
         watcher.EnableRaisingEvents = true;
         Console.WriteLine("Press q to quit");
-        Console.WriteLine($"[{NowString()}] Watching {downloadDir}");
-        while (Console.Read() != 'q') ;
-
+        Log.WriteLine($"Watching {downloadDir}");
+        while (Console.Read() != 'q')
+            ;
     }
+
     static void OnCreate(object source, FileSystemEventArgs e, Application app)
     {
-        Console.WriteLine($"[{NowString()}] File: {e.FullPath} {e.ChangeType}");
-        app.ProcessChange(e.FullPath);
-    }
-    static void OnChange(object source, FileSystemEventArgs e, Application app)
-    {
-        Console.WriteLine($"[{NowString()}] File: {e.FullPath} {e.ChangeType}");
+        Log.WriteLine($"File: {e.FullPath} {e.ChangeType}");
         app.ProcessChange(e.FullPath);
     }
 
-    static string NowString()
+    static void OnChange(object source, FileSystemEventArgs e, Application app)
     {
-        return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Log.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+        app.ProcessChange(e.FullPath);
     }
 }
